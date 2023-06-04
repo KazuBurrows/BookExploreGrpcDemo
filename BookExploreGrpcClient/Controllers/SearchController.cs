51cad5b0c7b4830b3b03c68dc89588b29a05be38
Kazuki Burrows <kazuiscool@gmail.com>
2023-06-04 19:01:30 +1200

    first commit

---
+++ BookExploreGrpcClient/Controllers/SearchController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BookExploreGrpcClient.Controllers
+{
+    public class SearchController : Controller
+    {
+        /// <summary>
+        /// "Starts the send and gets and outputs db response."
+        /// </summary>
+        /// <param name="param_search">"User search query input."</param>
+        /// <returns>"Table of book records from db."</returns>
+        //[OutputCache(NoStore = true, Duration = 0, VaryByParam = "*")]
+        //[OutputCache(NoStore = true, Duration = 0)]
+        [HttpGet]
+        public ActionResult Search(string param_search)
+        {
+
+
+            
+
+            return Json("Made it to search controller.", JsonRequestBehavior.AllowGet);
+        }
+    }
+}
---
+++ BookExploreGrpcClient/Models/SearchHandler.cs
@@ -0,0 +1,14 @@
+﻿using Grpc.Net.Client;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+
+namespace BookExploreGrpcClient.Models
+{
+    public class SearchHandler
+    {
+
+    }
+}
